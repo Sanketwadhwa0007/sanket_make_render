f42e11f6a82ea5b51c521a60720488b6082af3c1
Sanketwadhwa0007 <sanketwa7@gmail.com>
2025-02-01 15:32:01 +0530

    Updated the script with new changes

--- flak_render_make.py
+++ flak_render_make.py
@@ -4,8 +4,8 @@ app = Flask(__name__)
 
 @app.route("/", methods=["POST"])
 def run_code():
-    # data = request.json  # Get data from Make.com
-    result = {"message": "Hello, Make.com", "received": "data"}
+    data = request.json  # Get data from Make.com
+    result = {"message": "Hello, Make.com", "received": data}
     return jsonify(result)  # Send response
 
 if __name__ == "__main__":
