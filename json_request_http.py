3bb494b9eb3b51f65fd9d23b79142798180a9bca
Sanketwadhwa0007 <sanketwa7@gmail.com>
2025-02-01 20:59:35 +0530

    Updated the script with new changes

--- json_request_http.py
+++ json_request_http.py
@@ -1,6 +1,6 @@
 import requests
 
-url = "https://sanket-make-render.onrender.com"
+url = "https://sanket-make-render.onrender.com/data"
 data = {
     "title": "Hello World",
     "body": "This is a test post",
@@ -11,5 +11,11 @@ headers = {"Content-Type": "application/json"}
 
 response = requests.post(url, json=data, headers=headers)
 
+
 print(response.status_code)
+print(response.json())
+
+
+url = "https://sanket-make-render.onrender.com/hello"
+response = requests.get(url)
 print(response.json())
